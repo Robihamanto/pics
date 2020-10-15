fd544db3a6500df3e9dd23e5ff249b261284e442
robihamanto <rrobie44@gmail.com>
2020-10-15 08:01:24 +0700

    add unit test to network request

---
+++ lib/src/resources/hackernews_services.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'package:http/http.dart' show Client;
+import 'package:pics/src/models/item_model.dart';
+
+class NewsService {
+
+  var client = Client();
+  var _baseUrl = 'https://hacker-news.firebaseio.com/v0/';
+
+  fetchTopIDs() async {
+    final response = await client.get('$_baseUrl/topstories.json');
+    final ids = json.decode(response.body);
+    return ids;
+  }
+
+  fetchItemByID(int id) async {
+    final response = await client.get('$_baseUrl/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    final item = ItemModel.fromJson(parsedJson);
+    return item;
+  }
+
+}
+
---
+++ test/resources/hackernews_services_test.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:http/testing.dart';
+import 'package:pics/src/resources/hackernews_services.dart';
+
+void main() {
+
+  test("Fetch popular news", () async {
+    final service = NewsService();
+    service.client = MockClient((request) async {
+      return Response(json.encode([1,2,3,4]), 200);
+    });
+
+    final newsIDs = await service.fetchTopIDs();
+    expect(newsIDs, [1,2,3,4]);
+  });
+
+  test("Fetch single news", () async {
+    final service = NewsService();
+    service.client = MockClient((request) async {
+      final jsonMap = {'id': 123};
+      return Response(json.encode(jsonMap), 200);
+    });
+
+    final news = await service.fetchItemByID(123);
+    expect(news.id, 123);
+  });
+
+}
